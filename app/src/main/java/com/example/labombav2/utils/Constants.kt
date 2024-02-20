9b1a51bb33f11b1281bd628f2a02e656390f9c82
juniordeza25 <juniordezaramos@gmail.com>
2024-02-20 19:47:26 -0300

    feat: add function to retrieve topics with pagination

--- app/src/main/java/com/example/labombav2/utils/Constants.kt
+++ app/src/main/java/com/example/labombav2/utils/Constants.kt
@@ -6,10 +6,11 @@ object Constants {
     const val PLAYERS = "players"
     const val TOPICS = "topics"
     const val ID = "id"
-    const val NAME_PLAYER = "name"
+    const val NAME = "name"
     const val IS_CHECKED = "isChecked"
     const val ID_PLAYER = "idPlayer"
     const val COD_HTML_DOTS = "&#8226;"
     const val TITLE = "title"
     const val IMAGE = "image"
+    const val PAGE_SIZE = 10
 }
--- app/src/main/java/com/example/labombav2/utils/TopicDbManager.kt
+++ app/src/main/java/com/example/labombav2/utils/TopicDbManager.kt
@@ -3,6 +3,7 @@ package com.example.labombav2.utils
 import android.util.Log
 import com.example.labombav2.model.TopicModel
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 object TopicDbManager {
     private val db: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
@@ -19,13 +20,29 @@ object TopicDbManager {
             }
     }
 
-    private fun updateTopic(uid: String, id: String, updates: Map<String, Any>) {
+    fun updateTopic(uid: String, id: String, updates: Map<String, Any>) {
         userRef.document(uid).collection(Constants.TOPICS).document(id).update(updates)
             .addOnSuccessListener {
                 Log.d("TopicSuccessfullyUpdated", "The Topic was updated successfully")
             }
             .addOnFailureListener {
                 Log.e("ErrorUpdatingTopic", "Couldn't update the topic", it)
+            }
+    }
+
+//  Método para obtener una página de datos de Firestore
+    fun getTopicsPage(uid: String, numberPage: Int,
+    onSuccess: (QuerySnapshot) -> Unit, onFailure: (Exception) -> Unit) {
+        userRef.document(uid).collection(Constants.TOPICS)
+            .orderBy(Constants.NAME)
+            .limit(Constants.PAGE_SIZE.toLong())//Limitar el número de elementos por página
+            .startAfter(numberPage * Constants.PAGE_SIZE)//Comenzar después del último elemento de la página anterior
+            .get()
+            .addOnSuccessListener { snapshot ->
+                onSuccess(snapshot)
+            }
+            .addOnFailureListener { exception ->
+                onFailure(exception)
             }
     }
 }
