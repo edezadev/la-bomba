2c2ec49c1af516629194b850cca568273a1f6ff5
juniordeza25 <juniordezaramos@gmail.com>
2025-01-06 22:41:58 -0300

    feat: add chosen penalty to the game session

--- app/src/main/java/com/example/labombav2/controllers/adapters/PenaltyAdapter.kt
+++ app/src/main/java/com/example/labombav2/controllers/adapters/PenaltyAdapter.kt
@@ -1,17 +1,12 @@
 package com.example.labombav2.controllers.adapters
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.CheckBox
-import androidx.appcompat.widget.ListPopupWindow
 import androidx.recyclerview.widget.RecyclerView
 import com.example.labombav2.R
 import com.example.labombav2.databinding.ItemPenaltyBinding
 import com.example.labombav2.models.PenaltyModel
-import com.example.labombav2.utils.Constants
 import com.example.labombav2.config.auth.FirebaseAuthManager
 import com.example.labombav2.config.database.PenaltyDbManager
 import com.example.labombav2.utils.GameSession
@@ -19,20 +14,20 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class PenaltyAdapter(private var items: MutableList<PenaltyModel>) :
     RecyclerView.Adapter<PenaltyAdapter.ViewHolder>() {
-
-    private var selectedPenaltyId: String? = null
+    var uid: String? = null
+    init {
+//      Obtener UID una sola vez, para eliminar el penalty
+        FirebaseAuthManager.getUid {
+            uid = it
+        }
+    }
 
     inner class ViewHolder(binding: ItemPenaltyBinding):
         RecyclerView.ViewHolder(binding.root) {
         val cbPenalty = binding.cbPenalty
-        var uid: String? = null
         lateinit var penalty: PenaltyModel //Referencia al objeto(item) actual
 
         init {
-//          Obtener UID de usuario para eliminar el penalty
-            FirebaseAuthManager.getUid {
-                uid = it
-            }
 //          Establecer una vez el long click
             cbPenalty.setOnLongClickListener {
                 if (::penalty.isInitialized) {
@@ -49,9 +44,7 @@ class PenaltyAdapter(private var items: MutableList<PenaltyModel>) :
         return ViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
+    override fun getItemCount(): Int =  items.size
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -59,13 +52,13 @@ class PenaltyAdapter(private var items: MutableList<PenaltyModel>) :
         holder.penalty = item //almacenamos el objeto Penalty
         holder.cbPenalty.text = item.name
 
-        holder.cbPenalty.isChecked = (item.id == selectedPenaltyId)
+        holder.cbPenalty.isChecked = (item == GameSession.penalty)
 
         holder.cbPenalty.setOnClickListener {
-            if (item.id == selectedPenaltyId) {
-                selectedPenaltyId = null
+            if (item == GameSession.penalty) {
+                GameSession.penalty = null
             } else {
-                selectedPenaltyId = item.id
+                GameSession.penalty = item
 
                 notifyDataSetChanged()
             }
@@ -78,8 +71,15 @@ class PenaltyAdapter(private var items: MutableList<PenaltyModel>) :
             .setMessage(R.string.message_delete_penalty)
             .setPositiveButton(R.string.action_positive) { dialog, _ ->
 //              Eliminar de la BD
-                holder.uid?.let { PenaltyDbManager.deletePenalty(it, item.id) }
-                notifyItemRemoved(itemCount)
+                uid?.let {
+                    PenaltyDbManager.deletePenalty(it, item.id)
+                    GameSession.penalty = null
+                    val position = items.indexOf(item)
+                    if (position != -1) {
+                        items.removeAt(position)
+                        notifyItemRemoved(position)
+                    }
+                }
                 dialog.dismiss()
             }
             .setNegativeButton(R.string.action_negative) { dialog, _ ->
