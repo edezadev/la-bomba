4a0ee5bdbb1915ae6fa836ea79d60f74131b29b3
juniordeza25 <juniordezaramos@gmail.com>
2026-01-04 19:52:15 -0300

    feat: add rematch functionality

--- app/src/main/java/com/example/labombav2/controllers/activities/ResultsActivity.kt
+++ app/src/main/java/com/example/labombav2/controllers/activities/ResultsActivity.kt
@@ -8,8 +8,11 @@ import androidx.constraintlayout.widget.Group
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.labombav2.R
+import com.example.labombav2.config.auth.FirebaseAuthManager
+import com.example.labombav2.config.database.PlayerDbManager
 import com.example.labombav2.controllers.adapters.PlayerResultsAdapter
 import com.example.labombav2.databinding.ActivityResultsBinding
+import com.example.labombav2.models.LoserModel
 import com.example.labombav2.utils.BaseActivity
 import com.example.labombav2.utils.GameSession
 import com.google.android.material.appbar.MaterialToolbar
@@ -18,6 +21,7 @@ import com.google.android.material.button.MaterialButton
 class ResultsActivity : BaseActivity() {
     private var binding: ActivityResultsBinding? = null
     private var adapter: PlayerResultsAdapter? = null
+    private var potentialLosers = listOf<LoserModel>()
     private lateinit var toolbar: MaterialToolbar
     private lateinit var tvLoserAndPenalty: TextView
     private lateinit var recyclerResults: RecyclerView
@@ -47,6 +51,7 @@ class ResultsActivity : BaseActivity() {
 
 //        Creas un solo objeto lambda en memoria y pasas su referencia a ambos listeners.
         val goToHomeAction = { goToHome() }
+        btnRematch.setOnClickListener { goToRematch() }
         btnGoToHome.setOnClickListener { goToHomeAction() }
         btnGoToHomeSingle.setOnClickListener { goToHomeAction() }
     }
@@ -58,7 +63,7 @@ class ResultsActivity : BaseActivity() {
         val highestScore = loserList.maxOf { it.points }
 
 //        Filtra la lista para encontrar a todos los que tienen el puntaje más alto. Crea una lista
-        val potentialLosers = loserList.filter { it.points == highestScore }
+        potentialLosers = loserList.filter { it.points == highestScore }
 
         val message = if (potentialLosers.size == 1) {
 //            No hay empate, solo un perdedor
@@ -95,5 +100,31 @@ class ResultsActivity : BaseActivity() {
     private fun goToHome() {
         GameSession.reset() //Limpiar todos los datos del juego
         startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun goToRematch() {
+        /* .getUid es una operación asíncrona. Se ejecuta en segundo plano y llama a su lambda
+        ({ uid = it }) más tarde. Es más seguro poner toda la lógica dentgro del lambda */
+        FirebaseAuthManager.getUid { uid ->
+            /* Crear un conjunto (Set) con los jugadores que SÍ empataron.
+            * .map { it.player } transforma la List<LoserModel> en una List<PlayerModel>.
+            * .toSet() la convierte en un conjunto para búsquedas rápidas. */
+            val tiedLosers = potentialLosers.map { it.player }.toSet()
+
+    //        Iterar sobre TODOS los jugadores de la sesión original
+            for (player in GameSession.players) {
+                if (player !in tiedLosers) {
+                    PlayerDbManager.deletePlayer(uid, player.id)
+                }
+            }
+
+            /* Ya que no se llama a reset de GameSession por ser una revancha, solo se limpia
+            * directamente la lista de perdedores para que no se duplique con la nueva de la
+            * revancha*/
+            GameSession.loser.clear()
+
+            startActivity(Intent(this, SettingsActivity::class.java))
+            finish()
+        }
     }
 }
