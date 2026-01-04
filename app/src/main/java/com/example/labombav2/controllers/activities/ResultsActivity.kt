c9c5f598c2725d58673f3c6841a25f3630aa7627
juniordeza25 <juniordezaramos@gmail.com>
2026-01-04 17:40:01 -0300

    feat: Determine whether there is a single loser or a tie

--- app/src/main/java/com/example/labombav2/controllers/activities/ResultsActivity.kt
+++ app/src/main/java/com/example/labombav2/controllers/activities/ResultsActivity.kt
@@ -2,6 +2,8 @@ package com.example.labombav2.controllers.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.constraintlayout.widget.Group
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +19,7 @@ class ResultsActivity : BaseActivity() {
     private var binding: ActivityResultsBinding? = null
     private var adapter: PlayerResultsAdapter? = null
     private lateinit var toolbar: MaterialToolbar
+    private lateinit var tvLoserAndPenalty: TextView
     private lateinit var recyclerResults: RecyclerView
     private lateinit var btnRematch: MaterialButton
     private lateinit var btnGoToHome: MaterialButton
@@ -29,6 +32,7 @@ class ResultsActivity : BaseActivity() {
         setContentView(binding?.root)
         binding?.let {
             toolbar = it.toolbar
+            tvLoserAndPenalty = it.tvLoserAndPenalty
             recyclerResults = it.recyclerResults
             btnRematch = it.btnRematch
             btnGoToHome = it.btnGoToHome
@@ -39,17 +43,52 @@ class ResultsActivity : BaseActivity() {
         toolbar.title = getString(R.string.results_title)
         setupRecyclerView()
 
-
+        showLoserAndPenalty()
 
 //        Creas un solo objeto lambda en memoria y pasas su referencia a ambos listeners.
         val goToHomeAction = { goToHome() }
-        btnGoToHome.setOnClickListener { goToHomeAction }
-        btnGoToHomeSingle.setOnClickListener { goToHomeAction }
+        btnGoToHome.setOnClickListener { goToHomeAction() }
+        btnGoToHomeSingle.setOnClickListener { goToHomeAction() }
+    }
+
+    private fun showLoserAndPenalty() {
+        val loserList = GameSession.loser
+
+//        Busca el puntaje más alto
+        val highestScore = loserList.maxOf { it.points }
+
+//        Filtra la lista para encontrar a todos los que tienen el puntaje más alto. Crea una lista
+        val potentialLosers = loserList.filter { it.points == highestScore }
+
+        val message = if (potentialLosers.size == 1) {
+//            No hay empate, solo un perdedor
+            val loser = potentialLosers.first()
+            if (GameSession.penalty == null) {
+                getString(R.string.message_loser, loser.player.name)
+            } else {
+                "${getString(R.string.message_loser, loser.player.name)} " +
+                        "y tu castigo es ${GameSession.penalty?.name}"
+            }
+        } else { //Hay empate se muestran la opcion de empate
+            getString(R.string.message_tie)
+        }
+
+        tvLoserAndPenalty.text = message
+
+        val showRematch = potentialLosers.size > 1
+        if (showRematch) {
+            groupButtonChain.visibility = View.VISIBLE
+            btnGoToHomeSingle.visibility = View.GONE
+        } else {
+            groupButtonChain.visibility = View.GONE
+            btnGoToHomeSingle.visibility = View.VISIBLE
+        }
     }
 
     private fun setupRecyclerView() {
         adapter = PlayerResultsAdapter(GameSession.loser)
         recyclerResults.layoutManager = LinearLayoutManager(this)
+        recyclerResults.isNestedScrollingEnabled = false
         recyclerResults.adapter = adapter
     }
 
