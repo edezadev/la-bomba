4cfddc1ad4746917bda5e1898620ac7b4e708114
juniordeza25 <juniordezaramos@gmail.com>
2026-07-13 19:27:37 -0400

    chore: bump version code and name
    • Incremented `versionCode` to 3
    • Updated `versionName` to 1.0.2 in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.edeza.labomba"
         minSdk = 25
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // El manifest usará por defecto el ID de debug
